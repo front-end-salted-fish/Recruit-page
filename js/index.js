69e1542f4791eb19684e3bdacd0d2f6d0f7aec85
Chendabaiiii <532323969@qq.com>
2019-08-01 09:18:12 +0800

    update machine-learning

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(function() {
 	console.log('Welcome to TopView!');
 	// 旋转菜单模块
-	(() => {
+	!(() => {
 		const $home = $('#home');
 		const $imgs = $('#menu-list img');
 		let c = 130;
@@ -53,5 +53,68 @@ $(function() {
 			}
 			flag=!flag;
 		})
-	})()
+	})();
+	
+	// 机器学习
+	(() => {
+		let $machineDiv = $("#machine-learning");
+		let $headerFont = $($("#machine-learning .per-one .header-font")[0]);
+		let $spans = $("#machine-learning span");
+		let viewportHeight = $(".per-one .bg").height();
+		let $circle = $("#detail-pages circle");
+		let $paths = $("#detail-pages path");
+		let $lines = $("#detail-pages line");
+		let $svg = $($("#detail-pages svg")[0]);
+		let height = $svg.position().top + window.outerHeight - window.innerHeight + 50;
+		console.log($svg.position().top);
+		
+		function showSpan(obj){
+			obj.css({
+				opacity: 1,
+				top: "0",
+			});
+		}
+		
+		function hideSpan(obj) {
+			obj.css({
+				opacity: 0,
+				top: "30px",
+			});
+		}
+		
+		$machineDiv.on("scroll",function(){
+			$headerFont.css("opacity", (1 - $machineDiv.scrollTop() / 850));
+			// console.log($machineDiv.scrollTop());
+			if($machineDiv.scrollTop() > height) {
+				$circle.css("stroke","#000");
+				$paths.css("stroke","#000");
+				$lines.css("stroke","#000");
+			} else {
+				$circle.css("stroke","");
+				$paths.css("stroke","");
+				$lines.css("stroke","");
+			}
+			if($machineDiv.scrollTop() > 360) {
+				showSpan($($spans[0]));
+			} else {
+				hideSpan($($spans[0]));
+			}
+			if($machineDiv.scrollTop() > 410) {
+				showSpan($($spans[1]));
+			} else {
+				hideSpan($($spans[1]));
+			}
+			if($machineDiv.scrollTop() > 460) {
+				showSpan($($spans[2]));
+			} else {
+				hideSpan($($spans[2]));
+			}
+			if($machineDiv.scrollTop() > 510) {
+				showSpan($($spans[3]));
+			} else {
+				hideSpan($($spans[3]));
+			}
+
+		})
+	})();
 })
