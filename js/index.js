afba7d1a3db9b7542796d74142035139bf7d09c7
zarek <781568228@qq.com>
2019-08-02 08:55:50 +0800

    android update

--- js/index.js
+++ js/index.js
@@ -1,68 +1,133 @@
-$(function() {
+$(function () {
 	console.log('Welcome to TopView!');
 	// 旋转菜单模块
 	!(() => {
+		const $detailPages = $("#detail-pages")
 		const $home = $('#home');
 		const $imgs = $('#menu-list img');
+		const $menuList = $('#menu-list')
 		let c = 130;
 		let flag = true;
-		$imgs.on('click', function() {
-			$(this).css({
-				transition: ".5s linear",
-				transform: "scale(2) rotate(-720deg)",
-				opacity: 0.1
-			});
-			$(this).on("transitionend", end);
-		})
-		function end() {
-			$(this).css({
-				transition: ".1s",
-				transform: "scale(1) rotate(-720deg)",
-				opacity: 1
-			});	
-			$(this).off("transitionend",end);
-		}
-		//根据第三边和角度换算出坐标
-		//角度转弧度   角度*π/180 =弧度
-		const getPoint = (c,deg) => {
-			let a = Math.round(Math.sin(deg*Math.PI/180)*c);
-			let b = Math.round(Math.cos(deg*Math.PI/180)*c);
-			return {x:a,y:b};
-		}
-		$home.on('click', function() {
-			if(flag){
-				this.style.transform = "scale(1) rotate(-720deg)";
-				for(let i = 0;i< $imgs.length;i++){
-					$imgs.eq(i).css({
-						transform: "scale(1) rotate(-720deg)",
-						transition: ".5s "+(i*0.1)+"s ",
-						left: -getPoint(c, i*90/($imgs.length-1)).x+"px",
-						top: -getPoint(c, i*90/($imgs.length-1)).y+"px"
-					});
-				}
-			}else{
-				this.style.transform = "scale(1) rotate(0) ";	
-				for(let i = 0;i< $imgs.length;i++){
+
+		// 给五个菜单小图标绑定点击事件
+		$imgs.on('click', throttle(rotaImg, 500));
+		// 给菜单图标绑定点击事件
+		$home.on('click', throttle(rota, 1000));
+		// 给详情页绑定点击事件
+		$detailPages.on('click', function (ev) {
+			if (ev.target.id !== 'home' && !$(ev.target).hasClass('menu-item') && flag == false) {
+				$home.get(0).style.transform = "scale(1) rotate(0) ";
+				for (let i = 0; i < $imgs.length; i++) {
 					$imgs.eq(i).css({
 						transform: "scale(1) rotate(0)",
-						transition: ".5s "+(($imgs.length-1-i)*0.1)+"s ",
+						transition: ".5s " + (($imgs.length - 1 - i) * 0.1) + "s ",
+						left: 0,
+						top: 0
+					});
+				}
+				flag = true;
+			}	
+		})
+		
+		/*
+		 * @desc 函数节流 时间戳版
+		 * @param func 函数
+		 * @param wait 延迟执行毫秒数
+		 */
+		function throttle(func, wait) {
+			let previous = 0;
+			return function() {
+				let now = Date.now();
+				let context = this;
+				let args = arguments;
+				if (now - previous > wait) {
+					func.apply(context, args);
+					previous = now;
+				}
+			}
+		}
+		// 延迟停止时调用此函数
+		function end() {
+			$home.get(0).style.transform = "scale(1) rotate(0) ";
+				for (let i = 0; i < $imgs.length; i++) {
+					$imgs.eq(i).css({
+						transform: "scale(1) rotate(0)",
+						transition: ".3s " + (($imgs.length - 1 - i) * 0.1) + "s ",
+						left: 0,
+						top: 0
+					});
+				}
+				flag = true;
+				setTimeout(() => {
+					$(this).css({
+					transition: "0",
+					transform: "scale(1) rotate(-720deg)",
+					opacity: 1
+					})
+				},500)
+			$(this).off('transitionend', end);
+		}
+
+		//根据第三边和角度换算出坐标
+		//角度转弧度   角度*π/180 =弧度
+		const getPoint = (c, deg) => {
+			let a = Math.round(Math.sin(deg * Math.PI / 180) * c);
+			let b = Math.round(Math.cos(deg * Math.PI / 180) * c);
+			return {
+				x: a,
+				y: b
+			};
+		}
+		// 用于点击图片时调用的旋转函数
+		function rotaImg(ev) {
+			$(this).css({
+				transition: ".3s linear",
+				transform: "scale(2) rotate(-720deg)",
+				opacity: 0
+			});
+			$(this).on("transitionend", end);
+
+			ev.stopPropagation();
+			// return false;
+		}
+		// 用于点击home时调用的旋转函数
+		function rota(ev) {
+			if (flag) {
+				this.style.transform = "scale(1) rotate(-720deg)";
+				for (let i = 0; i < $imgs.length; i++) {
+					$imgs.eq(i).css({
+						transform: "scale(1) rotate(-720deg)",
+						transition: ".5s " + (i * 0.1) + "s ",
+						left: -getPoint(c, i * 90 / ($imgs.length - 1)).x + "px",
+						top: -getPoint(c, i * 90 / ($imgs.length - 1)).y + "px"
+					});
+				}
+			} else {
+				this.style.transform = "scale(1) rotate(0) ";
+				for (let i = 0; i < $imgs.length; i++) {
+					$imgs.eq(i).css({
+						transform: "scale(1) rotate(0)",
+						transition: ".5s " + (($imgs.length - 1 - i) * 0.1) + "s ",
 						left: 0,
 						top: 0
 					});
 				}
 			}
-			flag=!flag;
-		})
-	})()
+			flag = !flag;
+			ev.stopPropagation();
+			// return false;
+		}
+	
+	})();
 	//front-end
 	!(() => {
 		const $frontEnd = $("#front-end");
 		const $perOne = $(".per-one");
 		const $perTwo = $(".per-two");
-		$frontEnd.on("scroll", function(){
-			$perOne.css("opacity", (1-$frontEnd.scrollTop() / 2500));
-			if($frontEnd.scrollTop() > 1850) {
-				$perTwo.css("opacity", (1-($frontEnd.scrollTop()-1850) / 1000));
+		$frontEnd.on("scroll", function () {
+			$perOne.css("opacity", (1 - $frontEnd.scrollTop() / 2500));
+			if ($frontEnd.scrollTop() > 1850) {
+				$perTwo.css("opacity", (1 - ($frontEnd.scrollTop() - 1850) / 1000));
 			}
 		})
 	})();
@@ -71,19 +136,19 @@ $(function() {
 
 	// ios
 	(() => {
-	 const $ios = $("#ios");
-	 const $img1 = $(".pre-one>img");
-	 const $img2 = $(".pre-two>img");
-	 $ios.on("scroll", function () {
-		console.log($ios.scrollTop());
-		$img1.css("opacity", (1 - $ios.scrollTop() / 2500));
-		if($ios.scrollTop() > 1850) {
-		  $img2.css("opacity", (1 - ($ios.scrollTop() - 1850) / 1000));
-		}
-	  });
+		const $ios = $("#ios");
+		const $img1 = $(".pre-one>img");
+		const $img2 = $(".pre-two>img");
+		$ios.on("scroll", function () {
+			console.log($ios.scrollTop());
+			$img1.css("opacity", (1 - $ios.scrollTop() / 2500));
+			if ($ios.scrollTop() > 1850) {
+				$img2.css("opacity", (1 - ($ios.scrollTop() - 1850) / 1000));
+			}
+		});
 	})();
 
-	
+
 	// 机器学习
 	(() => {
 		let $machineDiv = $("#machine-learning");
@@ -95,50 +160,49 @@ $(function() {
 		let $lines = $("#detail-pages line");
 		let $svg = $($("#detail-pages svg")[0]);
 		let height = $svg.position().top + window.outerHeight - window.innerHeight + 50;
-		console.log($svg.position().top);
-		
-		function showSpan(obj){
+
+		function showSpan(obj) {
 			obj.css({
 				opacity: 1,
 				top: "0",
 			});
 		}
-		
+
 		function hideSpan(obj) {
 			obj.css({
 				opacity: 0,
 				top: "30px",
 			});
 		}
-		
-		$machineDiv.on("scroll",function(){
+
+		$machineDiv.on("scroll", function () {
 			$headerFont.css("opacity", (1 - $machineDiv.scrollTop() / 850));
 			// console.log($machineDiv.scrollTop());
-			if($machineDiv.scrollTop() > height) {
-				$circle.css("stroke","#000");
-				$paths.css("stroke","#000");
-				$lines.css("stroke","#000");
+			if ($machineDiv.scrollTop() > height) {
+				$circle.css("stroke", "#000");
+				$paths.css("stroke", "#000");
+				$lines.css("stroke", "#000");
 			} else {
-				$circle.css("stroke","");
-				$paths.css("stroke","");
-				$lines.css("stroke","");
+				$circle.css("stroke", "");
+				$paths.css("stroke", "");
+				$lines.css("stroke", "");
 			}
-			if($machineDiv.scrollTop() > 360) {
+			if ($machineDiv.scrollTop() > 360) {
 				showSpan($($spans[0]));
 			} else {
 				hideSpan($($spans[0]));
 			}
-			if($machineDiv.scrollTop() > 410) {
+			if ($machineDiv.scrollTop() > 410) {
 				showSpan($($spans[1]));
 			} else {
 				hideSpan($($spans[1]));
 			}
-			if($machineDiv.scrollTop() > 460) {
+			if ($machineDiv.scrollTop() > 460) {
 				showSpan($($spans[2]));
 			} else {
 				hideSpan($($spans[2]));
 			}
-			if($machineDiv.scrollTop() > 510) {
+			if ($machineDiv.scrollTop() > 510) {
 				showSpan($($spans[3]));
 			} else {
 				hideSpan($($spans[3]));
@@ -147,24 +211,22 @@ $(function() {
 		})
 	})();
 
-	// 伦哥模块
+	// android模块
 	!(() => {
 		// 获取页面元素
 		const $android = $('#android');
 		const $fontBg = $('.font-bg');
 		const $persOne = $('.pers-one');
 		// 监听滚动条事件
-		$android.on('scroll', function() {
-			// console.log($persOne.get(0).clientHeight)
-			// console.log(this.scrollTop)
-			let scale = this.scrollTop/$persOne.get(0).clientHeight - 1;
-			console.log(scale)
+		$android.on('scroll', function () {
+			let scale = this.scrollTop / $persOne.get(0).clientHeight - 1;
 			if (scale > 0) {
 				$fontBg.css({
-					opacity: 1-scale
+					opacity: 1 - scale
 				});
 			}
 		})
 	})();
+		
 
 });
