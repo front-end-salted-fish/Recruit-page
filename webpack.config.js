03d77cabc2eb1f255ce29eb42f58e8656592433d
Chendabaiiii <532323969@qq.com>
2020-03-08 14:22:59 +0800

    style: 修改轮播图和详情页样式以及图片

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    // mode: 'production',
+    // mode: 'development',
+    mode: 'production',
     entry: './js/index.js',
 
     output: {
